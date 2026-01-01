b2219010effd04f7470041daecd1b038edaa24bd
pimonputtalubnga <Masapimonpat@gmail.com>
2026-01-01 13:08:42 +0700

    Final

--- my-web/src/App.tsx
+++ my-web/src/App.tsx
@@ -1,13 +1,21 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/Layout/Header";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import AboutMe from "./pages/AboutMe";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import Competitions from "./pages/Competitions";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-white via-blue-50 to-blue-100 text-gray-800 font-sans antialiased">
-      <Header />
-      <main className="pt-24 px-6 max-w-6xl mx-auto transition-all duration-500">
-        <Outlet />
-      </main>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/competitions" element={<Competitions />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Layout>
   );
 }
